6200fce38b9f8bf2c460a2ff62e071af0a820e5d
Alexey Sosnin <morgreek@gmail.com>
2024-02-17 15:44:32 +0500

    prepare i18next

---
+++ slackchat/src/i18next.js
@@ -0,0 +1,16 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import resources from './locales/index.js';
+
+i18next
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: 'ru',
+        nterpolation: {
+            escapeValue: false,
+        },
+    });
+
+export default i18next;
---
+++ slackchat/src/locales/ru.js
@@ -0,0 +1,42 @@
+export default {
+    translation: {
+      logIn: 'Войти',
+      logOut: 'Выйти',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      noAppAccount: 'Нет аккаунта? ',
+      registration: 'Регистрация',
+      register: 'Зарегистрироваться',
+      areYouSure: 'Вы уверены?',
+      cancel: 'Отменить',
+      send: 'Отправить',
+      remove: 'Удалить',
+      rename: 'Переименовать',
+      
+      validation: {
+        mustUnique: 'Должно быть уникальным',
+        invalidLoginPassword: 'Неверные имя пользователя или пароль',
+        areUserExists: 'Такой пользователь уже существует',
+        from3To20Chars: 'От 3 до 20 символов',
+        least6Chars: 'Не менее 6 символов',
+        isRequiredField: 'Обязательное поле',
+        confirmPassword: 'Подтвердите пароль',
+        matchPasswords: 'Пароли должны совпадать',
+      },
+      
+      channels: {
+          channelsTitle: 'Каналы',
+          addChannel: 'Добавить канал',
+          renameChannel: 'Переименовать канал',
+          removeChannel: 'Удалить канал',
+
+      },
+
+      messages: {
+        messagesCount_one: '{{count}} сообщение',
+        messagesCount_few: '{{count}} сообщения',
+        messagesCount_many: '{{count}} сообщений',
+        enterMessage: 'Введите сообщение...',
+      },
+    },
+  };
