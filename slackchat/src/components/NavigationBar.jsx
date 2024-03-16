50a460ca0e46f5ac73676bc72bcef522c3ead239
Alexey Sosnin <morgreek@gmail.com>
2024-03-16 22:17:36 +0500

    fix link on brand name

--- slackchat/src/components/NavigationBar.jsx
+++ slackchat/src/components/NavigationBar.jsx
@@ -1,7 +1,9 @@
 import { Button, Container, Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 import useAuthorizationContext from '../hooks/useAuthorizationContext';
+import routes from '../routes';
 
 const NavigationBar = () => {
   const auth = useAuthorizationContext();
@@ -14,7 +16,7 @@ const NavigationBar = () => {
   return (
     <Navbar bg="white" className="shadow-sm" expand="lg">
       <Container>
-        <Navbar.Brand>Hexlet Chat</Navbar.Brand>
+        <Navbar.Brand as={Link} to={routes.chat}>Hexlet Chat</Navbar.Brand>
         {
           auth.userData
             ? <Button onClick={handleLogOut}>{t('logOut')}</Button>
